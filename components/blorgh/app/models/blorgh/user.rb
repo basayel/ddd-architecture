b2d19488bd4a9634ba4ce6930fb8982b83c7e366
Basayel Said <eng.basayel.said@gmail.com>
2023-02-27 08:21:43 +0000

    Testing Sonar code comments on PR

--- components/blorgh/app/models/blorgh/user.rb
+++ components/blorgh/app/models/blorgh/user.rb
@@ -4,5 +4,16 @@
 module Blorgh
   class User < Struct.new(:id, :name, :active)
     # business logic goes here
+    def foo(n, m)
+      case n
+      when 0
+        case m  # Noncompliant; nested case
+        when 0 then puts "0"
+          # ...
+        end
+      when 1 then puts "1"
+      else puts "2"
+      end
+    end
   end
 end
